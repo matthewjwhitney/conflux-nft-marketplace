4b810c64595e01143ee469afff8e3c876d4af93f
Matthew J. Whitney <matthewjwhitney@gmail.com>
2021-03-10 00:01:48 -0700

    add background and css libraries

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="nes-container is-dark is-rounded welcome-container">
+      <div className="row between-xs middle-xs">
+        <div className="column">
+          <span>Non Figgybles</span>
+        </div>
+        <div className="column">
+          <button className="nes-btn">Login</button>
+        </div>
+      </div>
     </div>
   );
 }
